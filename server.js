443d1b83586eab320cd90fdb0b39941dfe5c8b2e
hussam-200 <hussamibrahim434@gmail.com>
2025-07-05 20:14:59 +0300

    Flavor-Table-Deployment

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 pool.connect()
 .then(()=>{
-app.listen(Port, () => {
+app.listen(PORT, () => {
     console.log(`this is port web used ${PORT}`);
 })})
 .catch((error)=>{
